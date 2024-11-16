fb0bbefe5ad6ffedbe0a41cb019a8cdb18388c77
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-11-16 00:05:04 +0000

    Update Response.php

--- app/core/models/Response.php
+++ app/core/models/Response.php
@@ -3,11 +3,7 @@
 class Response extends \Prefab  {
     
     function json($status, $message=null){
-        if(empty($message) || is_null($message)){
-            $response = $status;
-        } else {
-            $response = array('status' => $status, 'message' => $message);
-        }
+        $response = array('status' => $status, 'message' => $message);
         
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($response);
